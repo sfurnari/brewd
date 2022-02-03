3cc3b231799ed5b3aee4326e0a8adba1cb0bcc37
Steven Furnari <s.furnari@hotmail.com>
2022-02-03 16:25:23 +1100

    some styling added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,22 @@ class User < ApplicationRecord
     has_many :roaster_ratings
     has_many :cafes, through: 'cafe_ratings'
     has_many :roasters, through: 'roaster_ratings'
+    has_many :following_relationships, class_name: 'Follow', foreign_key: 'follower_id'
+    has_many :followed_relationships, class_name: 'Follow', foreign_key: 'followed_id'
 
+    has_many :following, through: :following_relationships, source: :followed
+    has_many :followers, through: :followed_relationships, source: :follower
+
+    def follow_safe other_user
+        if self.following.include? other_user
+            return false
+        else
+            self.following << other_user
+            return true
+        end
+    end 
+
+    def following_users
+        User.where user: self.following        
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_052546) do
+ActiveRecord::Schema.define(version: 2022_02_03_034745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2022_02_01_052546) do
     t.string "name"
     t.string "address"
     t.integer "roaster_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
